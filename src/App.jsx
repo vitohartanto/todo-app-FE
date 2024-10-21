befcefe65d942d726a48ecb13ca8d365d7934a09
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-21 14:42:01 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,67 @@
-import InputTodo from './components/InputTodo';
-import TopComponent from './components/TopComponent';
-import ListTodos from './components/ListTodos';
 import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
+import TodoApp from './pages/TodoApp';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   const [description, setDescription] = useState('');
   const [editMode, setEditMode] = useState(null); // Menyimpan ID todo yang sedang diedit
   const [isDarkMode, setIsDarkMode] = useState(true); // State untuk mode
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   // Fungsi untuk toggle mode
   const toggleDarkMode = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
 
+  const handleLogin = (token) => {
+    // Simpan token di localStorage
+    localStorage.setItem('token', token);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    // Hapus token dari localStorage
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+  };
+
   return (
     <>
-      <div
-        className={`absolute h-full w-full z-[-3] ${
-          isDarkMode ? 'bg-[#161722]' : 'bg-[#e4e5f1]'
-        }`}
-      ></div>
-      <TopComponent isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-      <InputTodo
-        description={description}
-        setDescription={setDescription}
-        editMode={editMode}
-        setEditMode={setEditMode}
-        isDarkMode={isDarkMode}
-      />
-      <ListTodos
-        description={description}
-        setDescription={setDescription}
-        setEditMode={setEditMode}
-        isDarkMode={isDarkMode}
-      />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login onLogin={handleLogin} />} />
+            <Route
+              path="/todos"
+              element={
+                isAuthenticated ? (
+                  <TodoApp
+                    isDarkMode={isDarkMode}
+                    toggleDarkMode={toggleDarkMode}
+                    description={description}
+                    setDescription={setDescription}
+                    editMode={editMode}
+                    setEditMode={setEditMode}
+                    onLogout={handleLogout}
+                  />
+                ) : (
+                  <Navigate to="/login" />
+                )
+              }
+            />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
---
+++ src/pages/TodoApp.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import TopComponent from '../components/TopComponent';
+import InputTodo from '../components/InputTodo';
+import ListTodos from '../components/ListTodos';
+
+const TodoApp = ({
+  isDarkMode,
+  toggleDarkMode,
+  description,
+  setDescription,
+  editMode,
+  setEditMode,
+}) => {
+  return (
+    <>
+      {' '}
+      <div
+        className={`absolute h-full w-full z-[-3] ${
+          isDarkMode ? 'bg-[#161722]' : 'bg-[#e4e5f1]'
+        }`}
+      ></div>
+      <TopComponent isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      <InputTodo
+        description={description}
+        setDescription={setDescription}
+        editMode={editMode}
+        setEditMode={setEditMode}
+        isDarkMode={isDarkMode}
+      />
+      <ListTodos
+        description={description}
+        setDescription={setDescription}
+        setEditMode={setEditMode}
+        isDarkMode={isDarkMode}
+      />
+      ;
+    </>
+  );
+};
+
+export default TodoApp;
